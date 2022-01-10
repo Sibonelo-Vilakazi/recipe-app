d1e3bf1679457b527a0fdb254155d10f8a5e4a73
Sibonelo-Vilakazi <sibonelo@monkeyandriver.com>
2022-01-10 17:24:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bugarray-recipe-app';
+  public recipesData :Array<any>=[
+    {name:"Easy Sugar Cookies"},
+    {
+      name:"Easy Sugar Cookies"
+    },
+    {
+      name:"Sausage Balls"
+    },
+    {
+      name:"Raspberry and Almond Shortbread Thumbprints"
+    },
+    {
+      name:"Baked Macaroni and Cheese"
+    },
+
+    {
+      name:"Big Soft Ginger Cookies"
+    },
+
+  ]
+  ngOnInit(): void {
+      console.log(this.recipesData)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchBarComponent,
+    RecipeCardComponent,
   
   ],
   imports: [
